efa6823517c92a67e0e99e7de6ff2109cdd598b8
Kate <mhq0612@163.com>
2018-09-19 18:04:28 +0100

    testing

--- src/index.js
+++ src/index.js
@@ -2,4 +2,133 @@ function add(a, b){
   return a + b;
 }
 
-exports.add = add;
+
+function arrayOfString(array){
+
+  array.sort(function(a,b){
+    return a.length - b.length;
+  });
+  return array[array.length-1];
+
+}
+
+function encrypt(words){
+  const newWords=words.split("").map(letter => {
+    switch (letter) {
+    case "i","l":
+      return "1";
+    break;
+    case "z":
+      return "2";
+    break;
+
+    case "e":
+      return "3";
+    break;
+
+    case "a":
+      return "4";
+    break;
+
+    case "s":
+      return "5";
+    break;
+
+    case "g":
+      return "6";
+    break;
+
+    case "t","y":
+      return "7";
+    break;
+
+    case "b":
+      return "8";
+    break;
+
+    case "q":
+      return "9";
+    break;
+
+    case "o":
+      return "0";
+      break;
+    default:
+      return letter;
+  }
+  })
+  decodeWords=newWords.join("")
+return decodeWords;
+
+}
+
+function stringsAndNumbers(array){
+  let arrayNew = [];
+  for (let i=0; i<array.length; i++) {
+  if (arrayNew.indexOf(array[i])===-1){
+
+      arrayNew.push(array[i]);
+    }
+  }
+
+  return arrayNew;
+}
+
+function Developer(name,languages){
+  this.name = name;
+  this.languages = languages;
+
+}
+
+const developer=new Developer("nikki",["css", "html", "javascript"])
+
+Developer.prototype.learnLanguage =function(newLanguage) {
+  this.languages.push(newLanguage)
+  return this.languages;
+}
+
+const developerLearned=new Developer("nikki",["css", "html", "javascript"])
+
+function stringsConcat(array){
+  let newArray = [];
+  array.forEach(item=>{
+    if(typeof item ==="string"){
+     newArray.push(item);
+    }
+  });
+  return newArray.join(" ");
+}
+
+
+function negativeOnly(array){
+  let newArray=[];
+  array.forEach(number => {
+    if (number<0){
+      newArray.push(number);
+    }
+  })
+  return newArray;
+}
+
+function camelise(string){
+  //let newString ="";
+  const newArray=string.split(" ").map(word=>{
+    let first=word[0].toUpperCase();
+    return word=first +word.substring(1);
+  })
+
+
+  let firstLetter=newArray.join("")[0].toLowerCase();
+  return newString=firstLetter+newArray.join("").substring(1);
+}
+module.exports = {
+  add,
+  arrayOfString,
+  encrypt,
+  stringsAndNumbers,
+  developer,
+  developerLearned,
+  stringsConcat,
+  negativeOnly,
+  camelise
+}
--- test/index.test.js
+++ test/index.test.js
@@ -5,3 +5,58 @@ test('Addition', function(){
   const result = functions.add(2, 3);
   expect(result).toBe(expected);
 });
+
+test("longest string",function(){
+  const expected="hello world we are in london atm";
+  const result = functions.arrayOfString(["hello world we are in london atm","apple","another apple"]);
+  expect(result).toBe(expected);
+});
+
+test('decode text', function(){
+  const expected = "h3110";
+  const result = functions.encrypt("hello");
+  expect(result).toBe(expected);
+});
+
+test('unique string', function(){
+  const expected = ['a','b','c','d'];
+  const result = functions.stringsAndNumbers(['a','a','b','c','d','d']);
+  expect(result).toEqual(expected);
+});
+
+test("Developer", function(){
+  const expected = {
+    name: "nikki",
+    languages:["css", "html", "javascript"]
+    }
+  const result=functions.developer;
+  expect(result).toEqual(expected);
+});
+
+
+test("new language learned", function(){
+  const expected = ["css", "html", "javascript","python"];
+  const result = functions.developerLearned.learnLanguage("python");
+  expect (result).toEqual(expected);
+})
+
+
+test("string concat", function(){
+  const expected = "hello Nikki and Kate";
+  const result = functions.stringsConcat(["hello",333,"Nikki","and","Kate",123,44])
+  expect (result).toBe(expected);
+
+})
+
+test("negativeOnly", function(){
+  const expected = [-1, -3, -10, -111, -763];
+  const result = functions.negativeOnly([-1, 2, -3, 10, -10, 24,-111, 44,-763])
+  expect (result).toEqual(expected);
+})
+
+
+test("camelise strings",function(){
+  const expected="helloKateNikkiWedSunny";
+  const result = functions.camelise("hello kate nikki wed sunny");
+  expect (result).toEqual(expected);
+})
